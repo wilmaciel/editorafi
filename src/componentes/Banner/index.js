93ddcd347c966d123e2ae602a7ef09f7b5a19a5e
wilmaciel <wilmacielsilva@gmail.com>
2023-06-23 11:58:32 -0300

    Atualizações
    Banner

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -2,7 +2,7 @@ import './Banner.css'
 
 const Banner = () => {
     return (<header className="cabecalho">
-        <img src='/imagens/banner.png' alt='Logo do Organo'/>
+        <img src='/imagens/banner.png' alt='Logo Editora Fi'/>
     </header>)
 }
 
