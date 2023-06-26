c891ceb576f41e3f592f18647f1d0df2229b3f83
wilmaciel <wilmacielsilva@gmail.com>
2023-06-26 12:32:09 -0300

    Atualizações
    Colaboradores

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,9 +1,9 @@
-import { AiFillCloseCircle } from 'react-icons/ai';
 import './Colaborador.css'
+import { AiFillCloseCircle } from 'react-icons/ai';
 
 const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
     return (<div className="colaborador">
-        <AiFillCloseCircle size={25} className="deletar" onClick={aoDeletar}deletar />
+        <AiFillCloseCircle size={25} className="deletar" onClick={aoDeletar} />
         <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
             <img src={colaborador.imagem} alt={colaborador.nome} />
         </div>
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,9 +1,10 @@
 import Colaborador from '../Colaborador'
 import './Time.css'
 
-const Time = ({ time, colaboradores, aoDeletar }) => {
+const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
     return (
         colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
+            <input onChange={evento => mudarCor(evento.target.value, time.nome)} value={time.corSecundaria} type='color' className='input-cor'/>
             <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
             <div className='colaboradores'>
                 {colaboradores.map((colaborador, indice) => {
