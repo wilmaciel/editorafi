d41931b3e06e311c88fd1157f3a9ab6342648a81
wilmaciel <wilmacielsilva@gmail.com>
2023-06-27 09:53:41 -0300

    Atualizações
    Atualizações criticas para validação e adicionado painel de criação de time

--- src/App.js
+++ src/App.js
@@ -3,35 +3,43 @@ import Banner from "./componentes/Banner";
 import Formulario from "./componentes/Formulario";
 import Rodape from "./componentes/Rodape";
 import Time from "./componentes/Time";
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
 
   const [times, setTimes] = useState([
     {
+      id: uuidv4(),
       nome: 'Diagramador',
       cor: '#57C278'
     },
     {
+      id: uuidv4(),
       nome: 'Capista',
       cor: '#82CFFA'
     },
     {
+      id: uuidv4(),
       nome: 'Editor',
       cor: '#A6D157'
     },
     {
+      id: uuidv4(),
       nome: 'Comercial',
       cor: '#E06B69'
     },
     {
+      id: uuidv4(),
       nome: 'Diretoria',
       cor: '#DB6EBF'
     },
     {
+      id: uuidv4(),
       nome: 'Financeiro',
       cor: '#FFBA05'
     },
     {
+      id: uuidv4(),
       nome: 'SAC',
       cor: '#FF8A29'
     },
@@ -39,144 +47,168 @@ function App() {
 
   const inicial = [
     {
+      id: uuidv4(),
       nome: 'WILLIAN MACIEL',
       cargo: 'Advogado e aspirante de dev.',
       imagem: 'https://www.github.com/wilmaciel.png',
       time: times[0].nome
     },
     {
+      id: uuidv4(),
       nome: 'WILLIAN MACIEL',
       cargo: 'Advogado e aspirante de dev.',
       imagem: 'https://www.github.com/wilmaciel.png',
       time: times[0].nome
     },
     {
+      id: uuidv4(),
       nome: 'WILLIAN MACIEL',
       cargo: 'Advogado e aspirante de dev.',
       imagem: 'https://www.github.com/wilmaciel.png',
       time: times[0].nome
     },
     {
+      id: uuidv4(),
       nome: 'WILLIAN MACIEL',
       cargo: 'Advogado e aspirante de dev.',
       imagem: 'https://www.github.com/wilmaciel.png',
       time: times[0].nome
     },
     {
+      id: uuidv4(),
       nome: 'WILLIAN MACIEL',
       cargo: 'Advogado e aspirante de dev.',
       imagem: 'https://www.github.com/wilmaciel.png',
       time: times[1].nome
     },
     {
+      id: uuidv4(),
       nome: 'WILLIAN MACIEL',
       cargo: 'Advogado e aspirante de dev.',
       imagem: 'https://www.github.com/wilmaciel.png',
       time: times[1].nome
     },
     {
+      id: uuidv4(),
       nome: 'WILLIAN MACIEL',
       cargo: 'Advogado e aspirante de dev.',
       imagem: 'https://www.github.com/wilmaciel.png',
       time: times[1].nome
     },
     {
+      id: uuidv4(),
       nome: 'WILLIAN MACIEL',
       cargo: 'Advogado e aspirante de dev.',
       imagem: 'https://www.github.com/wilmaciel.png',
       time: times[1].nome
     },
     {
+      id: uuidv4(),
       nome: 'WILLIAN MACIEL',
       cargo: 'Advogado e aspirante de dev.',
       imagem: 'https://www.github.com/wilmaciel.png',
       time: times[2].nome
     },
     {
+      id: uuidv4(),
       nome: 'WILLIAN MACIEL',
       cargo: 'Advogado e aspirante de dev.',
       imagem: 'https://www.github.com/wilmaciel.png',
       time: times[2].nome
     },
     {
+      id: uuidv4(),
       nome: 'WILLIAN MACIEL',
       cargo: 'Advogado e aspirante de dev.',
       imagem: 'https://www.github.com/wilmaciel.png',
       time: times[2].nome
     },
     {
+      id: uuidv4(),
       nome: 'WILLIAN MACIEL',
       cargo: 'Advogado e aspirante de dev.',
       imagem: 'https://www.github.com/wilmaciel.png',
       time: times[2].nome
     },
     {
+      id: uuidv4(),
       nome: 'WILLIAN MACIEL',
       cargo: 'Advogado e aspirante de dev.',
       imagem: 'https://www.github.com/wilmaciel.png',
       time: times[3].nome
     },
     {
+      id: uuidv4(),
       nome: 'WILLIAN MACIEL',
       cargo: 'Advogado e aspirante de dev.',
       imagem: 'https://www.github.com/wilmaciel.png',
       time: times[3].nome
     },
     {
+      id: uuidv4(),
       nome: 'WILLIAN MACIEL',
       cargo: 'Advogado e aspirante de dev.',
       imagem: 'https://www.github.com/wilmaciel.png',
       time: times[3].nome
     },
     {
+      id: uuidv4(),
       nome: 'WILLIAN MACIEL',
       cargo: 'Advogado e aspirante de dev.',
       imagem: 'https://www.github.com/wilmaciel.png',
       time: times[3].nome
     },
     {
+      id: uuidv4(),
       nome: 'WILLIAN MACIEL',
       cargo: 'Advogado e aspirante de dev.',
       imagem: 'https://www.github.com/wilmaciel.png',
       time: times[4].nome
     },
     {
+      id: uuidv4(),
       nome: 'WILLIAN MACIEL',
       cargo: 'Advogado e aspirante de dev.',
       imagem: 'https://www.github.com/wilmaciel.png',
       time: times[4].nome
     },
     {
+      id: uuidv4(),
       nome: 'WILLIAN MACIEL',
       cargo: 'Advogado e aspirante de dev.',
       imagem: 'https://www.github.com/wilmaciel.png',
       time: times[4].nome
     },
     {
+      id: uuidv4(),
       nome: 'WILLIAN MACIEL',
       cargo: 'Advogado e aspirante de dev.',
       imagem: 'https://www.github.com/wilmaciel.png',
       time: times[4].nome
     },
     {
+      id: uuidv4(),
       nome: 'WILLIAN MACIEL',
       cargo: 'Advogado e aspirante de dev.',
       imagem: 'https://www.github.com/wilmaciel.png',
       time: times[5].nome
     },
     {
+      id: uuidv4(),
       nome: 'WILLIAN MACIEL',
       cargo: 'Advogado e aspirante de dev.',
       imagem: 'https://www.github.com/wilmaciel.png',
       time: times[5].nome
     },
     {
+      id: uuidv4(),
       nome: 'WILLIAN MACIEL',
       cargo: 'Advogado e aspirante de dev.',
       imagem: 'https://www.github.com/wilmaciel.png',
       time: times[5].nome
     },
     {
+      id: uuidv4(),
       nome: 'WILLIAN MACIEL',
       cargo: 'Advogado e aspirante de dev.',
       imagem: 'https://www.github.com/wilmaciel.png',
@@ -186,23 +218,31 @@ function App() {
 
   const [colaboradores, setColaboradores] = useState(inicial)
 
-  function deletarColaborador() {
-    console.log('Deletando um Colaborador');
+  function deletarColaborador(id) {
+    setColaboradores(colaboradores.filter(colaborador => colaborador.id !== id));
   }
 
-  function mudarCorDoTime(cor, nome) {
+  function mudarCorDoTime(cor, id) {
     setTimes(times.map(time => {
-      if(time.nome === nome) {
+      if(time.id === id) {
         time.cor = cor;
       }
       return time;
     }));
   }
 
+  function cadastrarTime(novoTime) {
+    setTimes([ ...times, { ...novoTime, id: uuidv4() }])
+  }
+
   return (
     <div>
       <Banner />
-      <Formulario times={times.map(time => time.nome)} aoCadastrar={colaborador => setColaboradores([...colaboradores, colaborador])} />
+      <Formulario
+        cadastrarTime={cadastrarTime} 
+        times={times.map(time => time.nome)} 
+        aoCadastrar={colaborador => setColaboradores([...colaboradores, colaborador])} 
+      />
       <section className="times">
         <h1>Minha organização</h1>
         {times.map((time, indice) => 
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -4,22 +4,49 @@ import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
-const Formulario = ({ aoCadastrar, times }) => {
+const Formulario = ({ aoCadastrar, times, cadastrarTime }) => {
   const [nome, setNome] = useState('')
   const [cargo, setCargo] = useState('')
   const [imagem, setImagem] = useState('')
   const [time, setTime] = useState('')
+  const [nomeTime, setNomeTime] = useState('')
+  const [corTime, setCorTime] = useState('')
 
   const aoSubmeter = (evento) => {
     evento.preventDefault()
-    console.log('form enviado', nome, cargo, imagem, time)
+
+    if (!validarCampos()) {
+      return
+    }
+
     aoCadastrar({
       nome,
       cargo,
       imagem,
       time,
     })
+
     limparCampos()
+  }
+
+  const validarCampos = () => {
+    if (!nome || !cargo || !imagem || !time) {
+      alert('Preencha todos os campos obrigatórios.')
+      return false
+    }
+
+    if (!validarFormatoImagem(imagem)) {
+      alert('A imagem deve ser uma URL válida.')
+      return false
+    }
+
+    return true
+  }
+
+  const validarFormatoImagem = (url) => {
+    // Expressão regular para validar o formato da imagem (URL)
+    const regex = /^(http(s)?:\/\/)([^\s/.]+\.([^\s]{2,}|[a-z]{2,})(\/[^\s]*)?)$/
+    return regex.test(url)
   }
 
   const limparCampos = () => {
@@ -46,6 +73,7 @@ const Formulario = ({ aoCadastrar, times }) => {
           aoAlterado={(valor) => setCargo(valor)}
         />
         <CampoTexto
+          obrigatorio={true}
           label="Imagem"
           placeholder="Informe o endereço da imagem"
           aoAlterado={(valor) => setImagem(valor)}
@@ -58,6 +86,26 @@ const Formulario = ({ aoCadastrar, times }) => {
           aoAlterado={(valor) => setTime(valor)}
         />
         <Botao texto="Criar Colaborador" />
+      </form>
+      <form className="formulario" onSubmit={(evento) => {
+        evento.preventDefault()
+        cadastrarTime({nome: nomeTime, cor: corTime })
+      }}>
+        <h2>Preencha os dados para criar um novo time.</h2>
+        <CampoTexto
+          obrigatorio
+          label="Nome do time"
+          valor={nomeTime}
+          aoAlterado={(valor) => setNomeTime(valor)}
+        />
+        <CampoTexto
+          obrigatorio
+          placeholder="#******"
+          label="Cor do time"
+          valor={corTime}
+          aoAlterado={(valor) => setCorTime(valor)}
+        />
+        <Botao texto="Criar um novo time" />
       </form>
     </section>
   )
